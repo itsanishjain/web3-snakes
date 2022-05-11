e72928cbec2ecf16b16ba6955a592014e56a0c70
anish jain <helloanishjain@gmail.com>
2022-05-11 23:11:59 +0530

    remove frontend

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer>
-      build by <a href="https://twitter.com/itsanishjain">Anish Jain</a>
-    </footer>
-  );
-}
